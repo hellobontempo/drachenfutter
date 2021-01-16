bdad1fe65e9afe4532c62c976a715e3711d13378
claire <clairebontempo@gmail.com>
2021-01-16 09:52:53 -0800

    model relationships

--- app/models/ingredient.rb
+++ app/models/ingredient.rb
@@ -1,2 +1,3 @@
 class Ingredient < ApplicationRecord
+    has_many :measurements
 end
--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -1,2 +1,7 @@
 class Recipe < ApplicationRecord
+    has_many :reviews
+    has_many :users, through: :reviews
+    has_many :measurements
+    has_many :ingredients, through: :measurements
+    
 end
