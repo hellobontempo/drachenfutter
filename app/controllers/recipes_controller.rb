aa3d07d4d64d1a6762d556a739f4163fc86679aa
claire <clairebontempo@gmail.com>
2021-01-17 12:34:50 -0800

    fixes recipe show page

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -1,5 +1,6 @@
 class RecipesController < ApplicationController
-  include RecipesHelper
+include RecipesHelper
+
   def new
   end
 
@@ -7,6 +8,7 @@ class RecipesController < ApplicationController
   end
 
   def show
+    @recipe = Recipe.find_by(params[:id])
   end
 
   def index
