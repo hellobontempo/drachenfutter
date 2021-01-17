80cc53f0e3b3f03e4e7112d2f351890ff8d05fe1
claire <clairebontempo@gmail.com>
2021-01-16 20:12:36 -0800

    sign up form complete

--- config/routes.rb
+++ config/routes.rb
@@ -15,5 +15,6 @@ Rails.application.routes.draw do
   get '/signup', to: 'users#new'
   post '/users/new', to: 'users#create'
   get 'users/show'
+  root 'welcome#home'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
