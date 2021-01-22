b6d70ba145b693886610114032aacd06fdfd0d05
claire <clairebontempo@gmail.com>
2021-01-22 08:58:05 -0800

    moves recipe grid to partial

--- app/helpers/recipes_helper.rb
+++ app/helpers/recipes_helper.rb
@@ -4,7 +4,9 @@ module RecipesHelper
         self.title.titlecase
     end
   
-
+    def link_to_image(image_path, target_link,options={})
+        link_to(image_tag(image_path, :border => "0"), target_link, options)
+    end
 
   
 end
