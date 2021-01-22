ddd995c1d45dc595cf3f2c71aa2076daa7dba376
claire <clairebontempo@gmail.com>
2021-01-21 18:14:55 -0800

    adds category attribute to recipe

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_01_21_223929) do
+ActiveRecord::Schema.define(version: 2021_01_22_020634) do
 
   create_table "ingredients", force: :cascade do |t|
     t.string "name"
@@ -36,6 +36,7 @@ ActiveRecord::Schema.define(version: 2021_01_21_223929) do
     t.datetime "updated_at", precision: 6, null: false
     t.string "photo"
     t.integer "creator_id"
+    t.string "category"
   end
 
   create_table "reviews", force: :cascade do |t|
