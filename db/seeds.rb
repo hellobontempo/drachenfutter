c282abe490a86ebfe1f6507532299f70bf7661c3
claire <clairebontempo@gmail.com>
2021-04-02 11:51:59 -0700

    changes seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -8,7 +8,7 @@
 require_relative 'api.rb'
 
 
-User.create(name: "Admin", email: "admin@me.com", username:  "admin", password: "123password")
+# User.create(name: "Admin", email: "admin@me.com", username:  "admin", password: "123password")
 
 150.times do
     create_random_recipe
@@ -18,19 +18,19 @@ end
 
 ##OPTIONAL SEED DATA FOR USER CONTENT##
 
-# user3 = User.create(name: Faker::Name.first_name , email: Faker::Internet.unique.email, username:  "lovinoven", password: "123password")
-# user4 = User.create(name: Faker::Name.first_name , email: Faker::Internet.unique.email, username:  "forkgetaboutit", password: "123password")
-# user5 = User.create(name: Faker::Name.first_name , email: Faker::Internet.unique.email, username:  "carbonator99", password: "123password")
+user3 = User.create(name: Faker::Name.first_name , email: Faker::Internet.unique.email, username:  "lovinoven", password: "123password")
+user4 = User.create(name: Faker::Name.first_name , email: Faker::Internet.unique.email, username:  "forkgetaboutit", password: "123password")
+user5 = User.create(name: Faker::Name.first_name , email: Faker::Internet.unique.email, username:  "carbonator99", password: "123password")
 
 
-# Review.create(user: user1, content: "So delicious!", rating: 3, recipe: recipe1)
-# Review.create(user: user2, content: "It was okay, too spicy!", rating: 1, recipe: recipe1)
-# Review.create(user: user3, content: "Blegh!", rating: 1, recipe: recipe1)
+Review.create(user: user1, content: "So delicious!", rating: 3, recipe: Recipe.find(1))
+Review.create(user: user2, content: "It was okay, too spicy!", rating: 1, recipe: Recipe.find(1))
+Review.create(user: user3, content: "Blegh!", rating: 1, recipe: Recipe.find(1))
 
-# Review.create(user: user4, content: "Yum!", rating: 5, recipe: recipe2)
-# Review.create(user: user5, content: "My mom loves this!", rating: 4, recipe: recipe2)
-# Review.create(user: user1, content: "My boyfriend dumped me after making him this!", rating: 1, recipe: recipe2)
+Review.create(user: user4, content: "Yum!", rating: 5, recipe: Recipe.find(2))
+Review.create(user: user5, content: "My mom loves this!", rating: 4, recipe: Recipe.find(2))
+Review.create(user: user1, content: "My boyfriend dumped me after making him this!", rating: 1, recipe: Recipe.find(2))
 
-# Review.create(user: user4, content: "Love it!", rating: 1, recipe: recipe3, favorite: true)
-# Review.create(user: user3, content: "Just incredible.", rating: 1, recipe: recipe4, favorite: true)
-# Review.create(user: user2, content: "So much flavor packed inside", rating: 1, recipe: recipe5, favorite: true)
+Review.create(user: user4, content: "Love it!", rating: 1, recipe: Recipe.find(3), favorite: true)
+Review.create(user: user3, content: "Just incredible.", rating: 1, recipe: Recipe.find(3), favorite: true)
+Review.create(user: user2, content: "So much flavor packed inside", rating: 3, recipe: Recipe.find(3), favorite: true)
