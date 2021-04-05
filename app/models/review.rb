750db68645a92c3cb1c51241dea11fb293a5f1b4
claire <clairebontempo@gmail.com>
2021-04-05 10:56:27 -0700

    changes query for favorite reviews"

--- app/models/review.rb
+++ app/models/review.rb
@@ -5,8 +5,9 @@ class Review < ApplicationRecord
   validates :rating, presence: true
   scope :order_by_date, -> {order(created_at: :desc)} 
   scope :order_by_recipe, -> {order(recipe_id: :desc)} 
-  scope :select_favorites, -> {where("favorite LIKE ?", 1)}
-  # scope :select_favorites, ->  {where("favorite @> ?", true)}
+  # scope :select_favorites, -> {where("favorite LIKE ?", true)}
+  scope :select_favorites, -> {where("favorite::boolean = ?", true)}
+
 
 
 
